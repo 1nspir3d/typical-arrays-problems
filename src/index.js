9e5f7e657e072cc321e7abb56c927e49dd651982
David <62627903+1nspir3d@users.noreply.github.com>
2020-09-30 02:50:41 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  let minNumber;
+    if (array === undefined || arguments.length === 0 || array.length === 0) {
+        minNumber = 0;        
+        return minNumber;
+    } else {
+        minNumber = array[0];
+    }
+      
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] < minNumber) {
+          minNumber = array[i];
+      }
+  }   
+  return minNumber;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  let minNumber; 
+    if ( array === undefined || arguments.length === 0 || array.length === 0) {
+        minNumber = 0;
+        return minNumber;
+    } else {
+        minNumber = array[0];
+    }
+      
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] > minNumber) {
+          minNumber = array[i];
+      }
+  }
+  return minNumber;
+
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let minNumber = 0;
+  if (array === undefined || arguments.length === 0 || array.length === 0) {
+      minNumber = 0;
+      return minNumber;
+  }
+  let i = 0;
+  for (i; i < array.length; i++) {
+      minNumber += array[i];
+  }
+  minNumber /= array.length;
+  return minNumber;
+};
